2222533a216dad95cd2372366686933c2c6b341b
Ami <1789105095@qq.com>
2025-01-13 15:00:53 +0800

    更新 公告.lua

--- 公告.lua
+++ 公告.lua
@@ -1,6 +1,10 @@
 
-if getAppVersion()<53 then
-diaLogs("发现新版本","1.修复翻译时造成文件报错").show()
+if getAppVersion()<55 then
+diaLogs("发现新版本","1.修复外部打开数据载入不全\n2.修复多选状态下越界无法扩选\n3.自定义关键词高亮语法重新修改")
+.setNegativeButton("下载","openUrl('https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1736751427433/RWMOD_10.9.apk')")
+--点击“关闭”不执行任何事件
+.setPositiveButton("关闭")
+.show()
 
 end
 
