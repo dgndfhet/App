20b0505cd333ad57449b4ca29f93590ca935edd8
Ami <1789105095@qq.com>
2025-01-13 15:59:00 +0800

    更新 公告.lua

--- 公告.lua
+++ 公告.lua
@@ -1,5 +1,5 @@
 
-if getAppVersion()<56 then
+if getAppVersion()<57 then
 diaLogs("发现新版本","1.修复外部打开数据载入不全\n2.修复多选状态下越界无法扩选\n3.自定义关键词高亮语法重新修改\n4.移除未及时删除的调试功能")
 .setNegativeButton("下载","openUrl('https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1736755038691/RWMOD_11.0.apk')")
 --点击“关闭”不执行任何事件
