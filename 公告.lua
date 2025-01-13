6d842857b5f31e15ffb13c891cf80e082254f467
Ami <1789105095@qq.com>
2025-01-13 15:58:45 +0800

    更新 公告.lua

--- 公告.lua
+++ 公告.lua
@@ -1,7 +1,7 @@
 
-if getAppVersion()<55 then
-diaLogs("发现新版本","1.修复外部打开数据载入不全\n2.修复多选状态下越界无法扩选\n3.自定义关键词高亮语法重新修改")
-.setNegativeButton("下载","openUrl('https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1736751427433/RWMOD_10.9.apk')")
+if getAppVersion()<56 then
+diaLogs("发现新版本","1.修复外部打开数据载入不全\n2.修复多选状态下越界无法扩选\n3.自定义关键词高亮语法重新修改\n4.移除未及时删除的调试功能")
+.setNegativeButton("下载","openUrl('https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1736755038691/RWMOD_11.0.apk')")
 --点击“关闭”不执行任何事件
 .setPositiveButton("关闭")
 .show()
