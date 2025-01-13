be8ea7394647037945f7384eee2604dfdc9c8168
Ami <1789105095@qq.com>
2025-01-13 16:00:59 +0800

    更新 公告.lua

--- 公告.lua
+++ 公告.lua
@@ -1,9 +1,14 @@
 
 if getAppVersion()<56 then
 diaLogs("发现新版本","1.修复外部打开数据载入不全\n2.修复多选状态下越界无法扩选\n3.自定义关键词高亮语法重新修改\n4.移除未及时删除的调试功能")
+
+.setNeutralButton("加入Q群",
+"openUrl('https://qm.qq.com/q/iYruxxYiyI')")
+
 .setNegativeButton("下载","openUrl('https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1736755038691/RWMOD_11.0.apk')")
---点击“关闭”不执行任何事件
+
 .setPositiveButton("关闭")
+
 .show()
 
 end
