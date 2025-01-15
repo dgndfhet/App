2d42c0fd57c4fc8cc184d5dfd63592283b80477d
Ami <1789105095@qq.com>
2025-01-15 12:25:09 +0800

    更新 公告.lua

--- 公告.lua
+++ 公告.lua
@@ -27,13 +27,19 @@ dialogs.show()
 ]]
 
 if getAppVersion()<58 then
-diaLogs("发现新版本","1.修复部位ini文件代码高亮失败\n2.修复代码中图像/音响无法快速预览")
+local dialogs=diaLogs("发现新版本","1.修复部位ini文件代码高亮失败\n2.修复代码中图像/音响无法快速预览")
 
-.setNeutralButton("历史更新",updateHistory)
+dialogs.setNeutralButton("历史更新",updateHistory)
 
-.setNegativeButton("下载","openUrl('https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1736878602374/RWMOD_11.1.apk')")
+dialogs.setNegativeButton("下载","openUrl('https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1736878602374/RWMOD_11.1.apk')")
 
-.setPositiveButton("关闭")
+dialogs.setPositiveButton("关闭")
+if getAppVersion()>56 then
+--57版本新增方法
+dialogs.setHtmlLoad(true)
+dialogs.setTextIsSelectable(true)
+dialogs.setPopupClosable(false)
+end
 
-.show()
+dialogs.show()
 end
