c3eab512bec56ad03adbc6a7c00f29053aa4a056
Ami <1789105095@qq.com>
2025-01-17 01:30:03 +0800

    更新 公告.lua

--- 公告.lua
+++ 公告.lua
@@ -1,4 +1,4 @@
-if getAppVersion()<60 then
+if getAppVersion()<61 then
 local dialogs=diaLogs("发现新版本","<center><img src='https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1737048061894/268989986.png'/></center><br/>"
 .."<b1./>优化初次使用配置包报错<br/><b>2.</b>优化配置包报错弹窗大量弹出，现在会一个一个展示<br/><b>3.</b>github文件访问镜像网站加速")
 
