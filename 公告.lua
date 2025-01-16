67260da0dd3f054a536071f468caaa12acdff73c
Ami <1789105095@qq.com>
2025-01-17 01:29:54 +0800

    更新 公告.lua

--- 公告.lua
+++ 公告.lua
@@ -1,6 +1,6 @@
 if getAppVersion()<60 then
 local dialogs=diaLogs("发现新版本","<center><img src='https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1737048061894/268989986.png'/></center><br/>"
-.."<b>1.</b>优化初次使用配置包报错<br/><b>2.</b>优化配置包报错弹窗大量弹出，现在会一个一个展示<br/><b>3.</b>github文件访问镜像网站加速")
+.."<b1./>优化初次使用配置包报错<br/><b>2.</b>优化配置包报错弹窗大量弹出，现在会一个一个展示<br/><b>3.</b>github文件访问镜像网站加速")
 
 dialogs.setNegativeButton("下载","openUrl('https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1737047438230/RWMOD_11.4.apk')")
 
