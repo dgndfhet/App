28da1d08231960d6ebb16df1b1d80767d5e1308f
Ami <1789105095@qq.com>
2025-01-17 00:08:57 +0800

    更新 公告.lua

--- 公告.lua
+++ 公告.lua
@@ -37,16 +37,9 @@ if getAppVersion()<58 then
 local dialogs=diaLogs("发现新版本","1.优化初次使用配置包报错\n2.优化配置包报错弹窗大量弹出，现在会一个一个展示\n3.修复配置包贴花节点关键词重复")
 
 dialogs.setNeutralButton("历史更新",updateHistory)
-
 dialogs.setNegativeButton("下载","openUrl('https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1737031396847/RWMOD_11.2.apk')")
 
 dialogs.setPositiveButton("关闭")
-if getAppVersion()>56 then
---57版本新增方法
 dialogs.setPopupClosable(false)
-end
-
 dialogs.show()
-else
---Toast("APP最新版")
 end
