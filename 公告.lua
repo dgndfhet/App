45345d6a8fb3563f783c42452e5884e57f9efe45
Ami <1789105095@qq.com>
2025-01-17 21:01:32 +0800

    更新 公告.lua

--- 公告.lua
+++ 公告.lua
@@ -4,10 +4,6 @@ local dialogs=diaLogs("发现新版本","1.优化初次使用配置包报错\n2.
 dialogs.setNegativeButton("下载","openUrl('https://fs-im-kefu.7moor-fs1.com/ly/4d2c3f00-7d4c-11e5-af15-41bf63ae4ea0/1737047438230/RWMOD_11.4.apk')")
 
 dialogs.setPositiveButton("关闭")
-if getAppVersion()>56 then
---57版本新增方法
-dialogs.setPopupClosable(false)
-end
 
 dialogs.show()
 end
